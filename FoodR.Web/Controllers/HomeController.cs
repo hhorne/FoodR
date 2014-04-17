3396816b922af9604bd6a81be17c69b3042d3200
Heath <heath@logic-bomb.com>
2014-04-17 08:57:44 -0400

    Basic ASP.NET MVC Site
    This will give us server side ability to perform actions such as auth
    and dynamic compression/bundling.

---
+++ FoodR.Web/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoodR.Web.Controllers
+{
+    public class HomeController : FoodRController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
