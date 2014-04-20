58bec618ffb1284a4b5d0cd17958b06103d513ef
Heath <heath@logic-bomb.com>
2014-04-20 14:21:13 -0400

    Setup FoodTruck model
    Created simple FoodTruck model class and a custom EntityConfig class for
    loading of Entity Type Configurations through MEF.

---
+++ FoodR.Web/Data/EntityConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.ModelConfiguration.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace FoodR.Web.Data.EntityTypeConfiguration
+{
+	public abstract class EntityConfig<T> :  EntityTypeConfiguration<T> where T : class
+	{
+		public void AddConfiguration(ConfigurationRegistrar registrar)
+		{
+			registrar.Add(this);
+		}
+	}
+
+	public interface IEntityConfig
+	{
+		void AddConfiguration(ConfigurationRegistrar registrar);
+	}
+}
--- FoodR.Web/Data/FoodRContext.cs
+++ FoodR.Web/Data/FoodRContext.cs
@@ -1,12 +1,18 @@
-﻿using FoodR.Web.Data.Models;
+﻿using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.Data.Entity;
+using System.Reflection;
+using FoodR.Web.Data.EntityTypeConfiguration;
+using FoodR.Web.Data.Models;
 using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace FoodR.Web.Data
 {
 	public class FoodRContext : IdentityDbContext<FoodRUser>
 	{
-        public FoodRContext()
-            : base("DefaultConnection", throwIfV1Schema: false) {
+		public DbSet<FoodTruck> FoodTrucks { get; set; }
+
+        public FoodRContext() : base("DefaultConnection", throwIfV1Schema: false) {
         }
 
 		static FoodRContext()
@@ -20,5 +26,25 @@ namespace FoodR.Web.Data
 		{
 			return new FoodRContext();
         }
+
+		protected override void OnModelCreating(DbModelBuilder modelBuilder)
+		{
+			ComposeModelConfiguration(modelBuilder);
+
+			base.OnModelCreating(modelBuilder);
+		}
+
+		private void ComposeModelConfiguration(DbModelBuilder modelBuilder)
+		{
+			var contextConfiguration = new ContextConfiguration();
+			var catalog = new AssemblyCatalog(Assembly.GetExecutingAssembly());
+			var container = new CompositionContainer(catalog);
+			container.ComposeParts(contextConfiguration);
+
+			foreach (var configuration in contextConfiguration.Configurations)
+			{
+				configuration.AddConfiguration(modelBuilder.Configurations);
+			}
+		}
 	}
 }
