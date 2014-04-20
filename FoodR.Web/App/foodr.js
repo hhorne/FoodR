ad5e7d9688252bec0e3c6b3e7886917f51509522
Snowman2334 <snowman2334@yahoo.com>
2014-04-19 22:45:14 -0400

    Added google maps api
    google maps is embedded in the map page, search works, as well as a
    simple list of trucks

--- FoodR.Web/App/foodr.js
+++ FoodR.Web/App/foodr.js
@@ -10,7 +10,7 @@ app.config(function($routeProvider) {
       controller:'LocateCtrl',
       templateUrl: '/Templates/locate.html'
     })
-    .when('/map/:zipcode', {
+    .when('/map/:address', {
       controller:'MapCtrl',
       templateUrl: '/Templates/map.html'
     })
@@ -40,7 +40,62 @@ app.controller('LocateCtrl', function($scope) {
 })
 
 app.controller('MapCtrl', function($scope, $routeParams) {
-	$scope.zipcode = $routeParams.zipcode;
+    $scope.address = $routeParams.address;
+    $scope.vendors = [
+        {
+            name: 'Happy Grilled Cheese',
+            id: 1,
+            lat: 30.332,
+            lng: -81.600
+        },
+        {
+            name: 'Chew Chew',
+            id: 2,
+            lat: 30.300,
+            lng: -81.650
+        }
+    ];
+
+    //google maps api stuff
+    $scope.geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(30.332, -81.655); //center of jacksonville
+    var mapOptions = {
+        zoom: 11,
+        center: latlng,
+        streetViewControl: false
+    }
+    $scope.map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+    angular.forEach($scope.vendors, function (vendor) {
+        var latlng = new google.maps.LatLng(vendor.lat, vendor.lng);
+        var marker = new google.maps.Marker({
+            position: latlng,
+            title: vendor.name,
+            map: $scope.map
+        });
+        
+        google.maps.event.addListener(marker, 'click', function () {
+            var infoWindow = new google.maps.InfoWindow({
+                content: vendor.name
+            });
+
+            infoWindow.open($scope.map, marker);
+        });
+    });
+
+    $scope.search = function() {
+        var address = document.getElementById("address").value;
+        $scope.geocoder.geocode({ 'address': address }, function (results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                $scope.map.setCenter(results[0].geometry.location);
+                $scope.map.setZoom(12);
+                var marker = new google.maps.Marker({
+                    map: $scope.map,
+                    position: results[0].geometry.location
+                });
+            }
+        });
+    }
 })
 
 app.controller('ScheduleCtrl', function($scope) {
@@ -70,3 +125,4 @@ app.controller('ScheduleCtrl', function($scope) {
 app.controller('LoginCtrl', function($scope) {
 	
 })
+
