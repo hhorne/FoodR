f2d5fb6eddbc7faf915dc4bc56a751f09054cebd
Heath <heath@logic-bomb.com>
2014-04-21 22:40:33 -0400

    Prepping for External Login providers

---
+++ FoodR.Web/ViewModels/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin.Security;
+
+namespace FoodR.Web.ViewModels
+{
+	public class LoginViewModel
+	{
+		public string ReturnUrl { get; set; }
+		public IEnumerable<AuthenticationDescription> LoginProviders { get; set; }
+	}
+}
