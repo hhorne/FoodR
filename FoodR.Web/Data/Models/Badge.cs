20320aed7eaac392c78b2f19cf4c33bec86c7a60
Heath <heath@logic-bomb.com>
2014-04-22 17:02:01 -0400

    Moved EarnedBadge into its own file

--- FoodR.Web/Data/Models/Badge.cs
+++ FoodR.Web/Data/Models/Badge.cs
@@ -11,13 +11,4 @@ namespace FoodR.Web.Data.Models
 		public string Name { get; set; }
 		public string Icon { get; set; }
 	}
-
-	public class EarnedBadge
-	{
-		public int EarnedBadgeId { get; set; }
-		public DateTime DateEarned { get; set; }
-		public int BadgeId { get; set; }
-		public virtual Badge Badge { get; set; }
-		public virtual FoodRUser User { get; set; }
-	}
 }
---
+++ FoodR.Web/Data/Models/EarnedBadge.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FoodR.Web.Data.Models
+{
+	public class EarnedBadge
+	{
+		public int EarnedBadgeId { get; set; }
+		public DateTime DateEarned { get; set; }
+		public int BadgeId { get; set; }
+		public virtual Badge Badge { get; set; }
+		public virtual FoodRUser User { get; set; }
+	}
+}
