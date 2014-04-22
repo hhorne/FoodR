05f23f286f21e897fcb7a7856ca30b44b179df96
Heath <heath@logic-bomb.com>
2014-04-22 17:19:50 -0400

    Created interfaces for messaging services
    Created IEmailService and ISmsService for classes that implement
    IIdentityMessageService so that Ninject can properly bind them to the
    needed implementations.

--- FoodR.Web/Services/SmsService.cs
+++ FoodR.Web/Services/SmsService.cs
@@ -3,12 +3,17 @@ using Microsoft.AspNet.Identity;
 
 namespace FoodR.Web.Services
 {
-	public class SmsService : IIdentityMessageService
+	public class SmsService : ISmsService
 	{
 		public Task SendAsync(IdentityMessage message)
 		{
 			// Plug in your sms service here to send a text message.
 			return Task.FromResult(0);
 		}
+	}
+
+	public interface ISmsService : IIdentityMessageService
+	{
+		Task SendAsync(IdentityMessage message);
 	}
 }
