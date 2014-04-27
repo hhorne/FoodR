f1a27f16f612fc8d3b58452231fd67df6dcc6e6d
Heath <heath@logic-bomb.com>
2014-04-27 17:20:56 -0400

    Made locations listed on Areas page into links
    Forgot to do this before commiting earlier. VERY IMPORTANT.

--- FoodR.Web/ViewModels/AreaDetailViewModel.cs
+++ FoodR.Web/ViewModels/AreaDetailViewModel.cs
@@ -16,6 +16,7 @@ namespace FoodR.Web.ViewModels
 		public int Id { get; set; }
 		public string Name { get; set; }
 		public string Description { get; set; }
+		public string UrlSlug { get; set; }
 		public Address Address { get; set; }
 	}
 }
