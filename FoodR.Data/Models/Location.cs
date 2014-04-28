f9a4b7762806ff6d4a789f10cf06d6fa726125bc
Heath <heath@logic-bomb.com>
2014-04-28 18:02:40 -0400

    addendum to last commit

--- FoodR.Data/Models/Location.cs
+++ FoodR.Data/Models/Location.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-
-namespace FoodR.Data.Models
+﻿namespace FoodR.Data.Models
 {
 	public class Location
 	{
@@ -12,14 +10,5 @@ namespace FoodR.Data.Models
 		public string StreetAddress { get; set; }
 		public string StreetAddress2 { get; set; }
 		public string ZipCode { get; set; }
-	}
-
-	public class Area
-	{
-		public int Id { get; set; }
-		public string Name { get; set; }
-		public string Description { get; set; }
-		public virtual ICollection<Location> Locations { get; set; }
-		public virtual City City { get; set; }
 	}
 }
