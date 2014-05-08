54b9b6e60b7e001d47b86ab720ae35033135e47f
Snowman2334 <snowman2334@yahoo.com>
2014-05-07 23:20:17 -0400

    Layout changes, login UI updates
    Using a new glyph package for the social media icons, we can switch this
    out later if its no good.

--- FoodR.Web/Controllers/Filters/UserAwareAttribute.cs
+++ FoodR.Web/Controllers/Filters/UserAwareAttribute.cs
@@ -1,4 +1,6 @@
-﻿using System.Web;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Web;
 using System.Web.Mvc;
 
 namespace FoodR.Web.Controllers.Filters
@@ -8,7 +10,18 @@ namespace FoodR.Web.Controllers.Filters
 		public override void OnActionExecuted(ActionExecutedContext filterContext)
 		{
 			filterContext.Controller.ViewBag.UserName = GetCurrentUserName();
+			filterContext.Controller.ViewBag.IsAdmin = GetCurrentUsersRoles();
 			base.OnActionExecuted(filterContext);
+		}
+
+		public bool GetCurrentUsersRoles()
+		{
+			if (HttpContext.Current.User.Identity.IsAuthenticated)
+			{
+				return HttpContext.Current.User.IsInRole("Admin");
+			}
+
+			return false;
 		}
 
 		public string GetCurrentUserName()
